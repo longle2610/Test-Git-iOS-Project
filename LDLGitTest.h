3e6a9f117e27cf76753f36eda93f29dafdaab421
Long Le <z_onlydragon_z@yahoo.com>
2014-08-06 16:28:57 -0700

    Added a test class

---
+++ LDLGitTest.h
@@ -0,0 +1,13 @@
+//
+//  LDLGitTest.h
+//  Git Test
+//
+//  Created by Long Le on 8/6/14.
+//  Copyright (c) 2014 Long Le. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LDLGitTest : NSObject
+
+@end
---
+++ LDLGitTest.m
@@ -0,0 +1,13 @@
+//
+//  LDLGitTest.m
+//  Git Test
+//
+//  Created by Long Le on 8/6/14.
+//  Copyright (c) 2014 Long Le. All rights reserved.
+//
+
+#import "LDLGitTest.h"
+
+@implementation LDLGitTest
+
+@end
